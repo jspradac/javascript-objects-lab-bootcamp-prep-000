85dabdaba8502823a2d9eee11679b75113757b2e
Samuel Prada <jspradac@gmail.com>
2018-08-28 12:35:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var recipes = {mole: "mole and rice"};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
